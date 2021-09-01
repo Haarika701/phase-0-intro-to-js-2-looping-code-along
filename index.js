7b68f4914b709f73cf1cf4e8ab25d0d99e44a705
HaarikaR <hramadugu@gmail.com>
2021-09-01 10:25:35 -0700

    Done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(names){
+    let occasion = [];
+    for(let i = 0; i< 3;i++){
+        occasion.push(`Thank you, ${names[i]}, for the wonderful surprise gift!` );
+    }
+    return occasion;
+}
+function countDown(){
+    let countup = 0;
+while (countup < 11) {
+  console.log(countup++);
+}
+}
+countDown(11);
